4885444df18d27abb6dc193a1085b01297df6d2b
Mark Holton <holtonma@Macintosh-69.local>
2008-11-25 20:49:00 -0800

    sample started to store to database, check for duplicates before storing (MySQL sample)

---
+++ sample_store_to_db.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby -w
+# created by Mark Holton  (holtonma@gmail.com)
+# license: copy this code as much as you want to
+# 11-25-2008
+# purpose: sample of code that uses carl_spackler to grab tournament data, and update a MySQL database
+# other database schemas that differ from this one will require modifications
