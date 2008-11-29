2e658d1f7048028fe1d8d187ad335d213738d042
Mark Holton <holtonma@Macintosh-69.local>
2008-11-28 19:52:55 -0800

    improved test_scrape_pga_tour such that it will check the mock for exact results... would be nice to perform some assert_matches using RegEx's of the score, name, etc data

--- test_carl_spackler.rb
+++ test_carl_spackler.rb
@@ -9,14 +9,14 @@ class TestCarlSpackler < Test::Unit::TestCase
   
   def setup
     # mocking this out to not hit the network...
-    pga = open("sample.html") { |f| Nokogiri(f) }
+    pga = open("alt-1.html") { |f| Nokogiri(f) }
     @test_leaderboard = Nokogiri(pga.to_html).to_s
   end
   
   def test_open_mock_file
-    pga = open("sample.html") { |f| Nokogiri(f) }
+    pga = open("alt-1.html") { |f| Nokogiri(f) }
     doc = Nokogiri(pga.to_html)
-    assert_equal 79180, doc.to_s.length #make sure we have the correct file
+    assert_equal 157803, doc.to_s.length #make sure we have the correct file
     assert_equal Nokogiri::HTML::Document, doc.class
       #  everytime Nokogiri::HTML(open(url)) is called inside of 'carl_spackler', 
       #  return the mock sample, instead of hitting the network
@@ -29,10 +29,35 @@ class TestCarlSpackler < Test::Unit::TestCase
       @test_leaderboard
     }
     players = pga.fetch(url) 
-    assert_equal 50, players.length
-    assert_equal 1, players[0].rank
-    assert_equal "Tiger", players[0].fname
-    assert_equal "Woods", players[0].lname
+    assert_equal 71, players.length
+    assert_equal 12, players[0].length
+    assert_equal 12, players[1].length
+    # headers is row 1
+    assert_equal "Pos.", players[0][1]
+    assert_equal "Start", players[0][2]
+    assert_equal "Player", players[0][3]
+    assert_equal "Today", players[0][4]
+    assert_equal "Thru", players[0][5]
+    assert_equal "To Par", players[0][6]
+    assert_equal "R1", players[0][7]
+    assert_equal "R2", players[0][8]
+    assert_equal "R3", players[0][9]
+    assert_equal "R4", players[0][10]
+    assert_equal "Total", players[0][11]
+    # 9th player row
+    assert_equal "30", players[8][0]
+    assert_equal "T7", players[8][1]
+    assert_equal "T3", players[8][2]
+    assert_equal "Dustin Johnson (PB)", players[8][3]
+    assert_equal "1", players[8][4]
+    assert_equal "F", players[8][5]
+    assert_equal "-6", players[8][6]
+    assert_equal "73", players[8][7]
+    assert_equal "68", players[8][8]
+    assert_equal "68", players[8][9]
+    assert_equal "73", players[8][10]
+    assert_equal "282", players[8][11]
+    assert_equal nil, players[8][12]
   end
   
   def test_friendly_structure
@@ -45,22 +70,8 @@ class TestCarlSpackler < Test::Unit::TestCase
     #players.each do |p|
       #puts "#{p.pos} :: [#{p.name}] #{p.fname} #{p.lname} #{p.start} #{p.thru} #{p.to_par} (#{p.r1} #{p.r2} #{p.r3} #{p.r4})"
     #end
-    assert_equal "bite me",  players[1].fname
-    assert_equal 130, players.length
-  end
-  
-  def test_mercedes
-    pga = PGA.new
-    url = "http://www.pgatour.com/leaderboards/current/r060/alt-1.html"
-    flexmock(pga).should_receive(:open).with(url).and_return{
-      @test_leaderboard
-    }
-    players = pga.friendly_structure(pga.fetch(url, true))
-    #players.each do |p|
-      #puts "#{p.pos} :: [#{p.name}] #{p.fname} #{p.lname} #{p.start} #{p.thru} #{p.to_par} (#{p.r1} #{p.r2} #{p.r3} #{p.r4})"
-    #end
-    assert_equal "foo bar baz",  players[1].fname
-    assert_equal 31, players.length
+    assert_equal "Steve",  players[1].fname
+    assert_equal 182, players.length
   end
   
   def test_08_urls
