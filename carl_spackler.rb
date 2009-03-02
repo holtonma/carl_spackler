538ef7413d5fff446eb2de734c18a6d493ea2c75
Mark Holton <holtonma@Macintosh-69.local>
2009-03-01 22:07:31 -0800

    added tourney

--- carl_spackler.rb
+++ carl_spackler.rb
@@ -92,7 +92,7 @@ module CARL_SPACKLER
                 }
       elsif year == 2009
         urls = %w(
-                  r016 r006 r002 r003 r004 r005 r007
+                  r016 r006 r002 r003 r004 r005 r007 r457
                 ).map { |t|
                   "http://www.pgatour.com/leaderboards/current/#{t}/alt-1.html"
                 }
